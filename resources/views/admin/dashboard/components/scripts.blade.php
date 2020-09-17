e95be8bae91e09fe0f3ad846332423b68aa9c7ec
Ian Sebastian <53462336+igmsebastian-commude@users.noreply.github.com>
2020-09-17 12:16:43 +0800

    Removed JS Vendors directory

--- resources/views/admin/dashboard/components/scripts.blade.php
+++ resources/views/admin/dashboard/components/scripts.blade.php
@@ -1,4 +1,4 @@
-<script src={{ asset('js/vendors/jquery-3.3.1.min.js') }}></script>
-<script src={{ asset('js/vendors/dataTables.min.js') }}></script>
-<script src={{ asset('js/vendors/dataTables.select.min.js') }}></script>
+<script src={{ asset('js/jquery-3.3.1.min.js') }}></script>
+<script src={{ asset('js/dataTables.min.js') }}></script>
+<script src={{ asset('js/dataTables.select.min.js') }}></script>
 <script src={{ asset('js/app.js') }}></script>
