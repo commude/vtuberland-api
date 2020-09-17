a5ef05d823c27ec62fef32ef47592f06f30ccde2
Ian Sebastian <53462336+igmsebastian-commude@users.noreply.github.com>
2020-09-17 09:10:53 +0800

    Admin Rename Transaction to Purchase

--- app/Http/Controllers/Admin/Web/PurchaseController.php
+++ app/Http/Controllers/Admin/Web/PurchaseController.php
@@ -4,9 +4,8 @@ namespace App\Http\Controllers\Admin\Web;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\Transaction;
+use App\Models\Purchase;
 use App\Http\Resources\Bases\DataTableResource as DataTable;
-use App\Http\Resources\TransactionResource;
 
 class PurchaseController extends Controller
 {
@@ -20,28 +19,28 @@ class PurchaseController extends Controller
 
     /**
      * Return list of purchases by Datatable's request
-     * 
+     *
      * @param  \Illuminate\Http\Request $request
      * @return string
      */
     public function purchaseList(Request $request)
     {
         $search = $request->input('columns')[2]['search']['value'] ?? '';
-        $purchases = Transaction::offset($request->input('start'))
+        $purchases = Purchase::offset($request->input('start'))
                                 ->whereHasSearchFor('user', 'name', $search)
                                 ->limit($request->input('length'))
-                                ->orderBy('transactions.created_at')
+                                ->orderBy('purchases.created_at')
                                 ->get();
 
         // Get all count from table.
-        $totalCount = Transaction::count();
+        $totalCount = Purchase::count();
 
         // Get all Filtered count from table.
-        $totalFiltered = Transaction::whereHasSearchFor('user', 'name', $search)
+        $totalFiltered = Purchase::whereHasSearchFor('user', 'name', $search)
                                     ->count();
 
         // Get total price
-        $priceList = Transaction::whereHasSearchFor('user', 'name', $search)->get();
+        $priceList = Purchase::whereHasSearchFor('user', 'name', $search)->get();
         $totalPrice = 0;
         foreach ($priceList as $eachPrice){
             $totalPrice += $eachPrice->spotCharacter[0]->character->price;
@@ -57,6 +56,6 @@ class PurchaseController extends Controller
             ];
         }
 
-        return new DataTable($purchaseList, $totalCount, $totalFiltered, $totalPrice); 
+        return new DataTable($purchaseList, $totalCount, $totalFiltered, $totalPrice);
     }
 }
