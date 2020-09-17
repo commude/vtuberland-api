390dd7398a11269bea80aa4eaa4d6d821961a3c9
AkiraHosoda-nokisaki <histukilab@gmail.com>
2020-09-17 15:38:08 +0900

    購入者詳細：実装完了

--- resources/views/admin/dashboard/buyers/users.blade.php
+++ resources/views/admin/dashboard/buyers/users.blade.php
@@ -4,7 +4,7 @@
 
 @section('contents')
 <section class="dashboardPageActionSec dashboard02">
-    <div class="dashboardPageActionSec__bar"><a class="btn-backToDashboard" href="../dashboard_02.html">一覧へもどる</a></div>
+    <div class="dashboardPageActionSec__bar"><a class="btn-backToDashboard" href="{{ route('admin.index') }}">一覧へもどる</a></div>
 </section>
 
 <section class="dashboardDetailPage__detailCont">
@@ -27,36 +27,13 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach ($user->spotCharacters as $character)
                     <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
+                        <td class="detailContInner__itemDateTime">{{ $character->created_at }}</td>
+                        <td class="detailContInner__itemPurchaseContent">{{ $character->character->name }}</td>
+                        <td class="detailContInner__itemPurchasePrice">{{ $character->character->price }}</td>
                     </tr>
-                    <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
-                    </tr>
-                    <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
-                    </tr>
-                    <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
-                    </tr>
-                    <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
-                    </tr>
-                    <tr class="detailContInner__row">
-                        <td class="detailContInner__itemDateTime">購入日時</td>
-                        <td class="detailContInner__itemPurchaseContent">購入コンテンツ</td>
-                        <td class="detailContInner__itemPurchasePrice">購入金額</td>
-                    </tr>
+                    @endforeach
                 </tbody>
             </table>
         </div>
